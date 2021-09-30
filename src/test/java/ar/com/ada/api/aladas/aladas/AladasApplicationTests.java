3068091627534e4585a48f195f1f24de8846e4a8
Muriel3000 <murielmartinacorrea@gmail.com>
2021-09-30 17:53:57 -0300

    Se agregan tests de validaciones

--- src/test/java/ar/com/ada/api/aladas/aladas/AladasApplicationTests.java
+++ src/test/java/ar/com/ada/api/aladas/aladas/AladasApplicationTests.java
@@ -1,5 +1,6 @@
 package ar.com.ada.api.aladas.aladas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,9 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ar.com.ada.api.aladas.entities.Aeropuerto;
+import ar.com.ada.api.aladas.entities.Usuario;
 import ar.com.ada.api.aladas.entities.Vuelo;
 import ar.com.ada.api.aladas.entities.Vuelo.EstadoVueloEnum;
+import ar.com.ada.api.aladas.security.Crypto;
+import ar.com.ada.api.aladas.services.AeropuertoService;
 import ar.com.ada.api.aladas.services.VueloService;
+import ar.com.ada.api.aladas.services.AeropuertoService.ValidacionAeropuertoDataEnum;
 import ar.com.ada.api.aladas.services.VueloService.ValidacionVueloDataEnum;
 
 @SpringBootTest
@@ -20,6 +26,8 @@ class AladasApplicationTests {
 	@Autowired
 	VueloService vueloService;
 
+	@Autowired
+	AeropuertoService aeropuertoService;
 
 	@Test
 	void vueloTestPrecioNegativo() {
@@ -30,10 +38,92 @@ class AladasApplicationTests {
 	}
 
 	@Test
-	void vueloTestOk(){
+	void vueloTestPrecioOk(){
 		Vuelo vueloConPrecioOk = new Vuelo();
 		vueloConPrecioOk.setPrecio(new BigDecimal(100));
 		assertTrue(vueloService.validarPrecio(vueloConPrecioOk));
+	}
+
+	@Test
+	void aeropuertoValidarCodigoIATAOK() {
+		// From Florencia Di Felice to Everyone: 07:42 PM
+		// el código no debe llevar número y sólo 3 letras, así que habría que limitarlo
+		// a eso, no?
+
+		String codigoIATAOk1 = "EZE";
+		String codigoIATAOk2 = "AEP";
+		String codigoIATAOk3 = "NQN";
+		String codigoIATAOk4 = "N  ";
+		String codigoIATAOk5 = "N39";
+
+		/*
+		 * //String codigoIATAOk4 = "N  ";
+		 * 
+		 * //En este caso, afirmo que espero que el length del codigoIATAOk1 sea 3
+		 * assertEquals(3, codigoIATAOk1.length());
+		 * 
+		 * //En este caso, afirmo que espero qeu el resultado de la condicion //sea
+		 * verdaderro(en este caso, lenght == 3) assertTrue(codigoIATAOk2.length() ==
+		 * 3);
+		 * 
+		 * //assertTrue(codigoIATAOk4.length() == 3);
+		 */
+
+		Aeropuerto aeropuerto1 = new Aeropuerto();
+		aeropuerto1.setCodigoIATA(codigoIATAOk1);
+
+		Aeropuerto aeropuerto2 = new Aeropuerto();
+		aeropuerto2.setCodigoIATA(codigoIATAOk2);
+
+		Aeropuerto aeropuerto3 = new Aeropuerto();
+		aeropuerto3.setCodigoIATA(codigoIATAOk3);
+
+		Aeropuerto aeropuerto4 = new Aeropuerto();
+		aeropuerto4.setCodigoIATA(codigoIATAOk4);
+
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto1));
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto2));
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto3));
+
+		assertFalse(aeropuertoService.validarCodigoIATA(aeropuerto4));
+
+	}
+
+	@Test
+	void aeropuertoValidarCodigoIATANoOK() {
+		// From Florencia Di Felice to Everyone: 07:42 PM
+		// el código no debe llevar número y sólo 3 letras, así que habría que limitarlo
+		// a eso, no?
+
+	}
+
+	@Test
+	void vueloVerificarValidacionAeropuertoOrigenDestino() {
+		// En este validar todas las posibilidades de si el aeropuerto
+		// origen es igual al de destion o todo lo que se les ocurra
+	}
+
+	@Test
+	void vueloChequearQueLosPendientesNoTenganVuelosViejos() {
+
+		// Queremos validar que cuando hagamos un metodo que traiga los vuelos actuales
+		// para
+		// hacer reservas, no haya ningun vuelo en el pasado.
+	}
+
+	@Test
+	void vueloVerificarCapacidadMinima() {
+
+	}
+
+	@Test
+	void vueloVerificarCapadidadMaxima() {
+
+	}
+
+	@Test
+	void aeropuertoTestBuscadorIATA() {
+
 	}
 
 	@Test
@@ -47,6 +137,51 @@ class AladasApplicationTests {
 		assertTrue(vueloService.validarAeropuertoOrigenDifDestino(vuelo));
 	}
 
+	@Test
+	void testearEncriptacion() {
+
+		String contraseñaImaginaria = "pitufosasesinos";
+		String contraseñaImaginariaEncriptada = Crypto.encrypt(contraseñaImaginaria, "palabra");
+
+		String contraseñaImaginariaEncriptadaDesencriptada = Crypto.decrypt(contraseñaImaginariaEncriptada, "palabra");
+
+		// assertTrue(contraseñaImaginariaEncriptadaDesencriptada.equals(contraseñaImaginaria));
+		assertEquals(contraseñaImaginariaEncriptadaDesencriptada, contraseñaImaginaria);
+	}
+
+	@Test
+	void testearContraseña() {
+		Usuario usuario = new Usuario();
+
+		usuario.setUsername("Diana@gmail.com");
+		usuario.setPassword("qp5TPhgUtIf7RDylefkIbw==");
+		usuario.setEmail("Diana@gmail.com");
+
+		assertFalse(!usuario.getPassword().equals(Crypto.encrypt("AbcdE23", usuario.getUsername().toLowerCase())));
+
+	}
+
+	@Test
+	void testearAeropuertoId(){
+		Aeropuerto aeropuerto = new Aeropuerto();
+		aeropuerto.setAeropuertoId(117);
+		aeropuerto.setCodigoIATA("MDZ");
+		aeropuerto.setNombre("Mendoza");
+
+		assertEquals(ValidacionAeropuertoDataEnum.ERROR_AEROPUERTO_YA_EXISTE, aeropuertoService.validar(aeropuerto));
+	}
+
+
+	@Test
+	void testearAeropuertoCodigoIATA(){
+		Aeropuerto aeropuerto = new Aeropuerto();
+		aeropuerto.setAeropuertoId(17);
+		aeropuerto.setCodigoIATA("  M");
+		aeropuerto.setNombre("Mendoza");	
+
+		assertEquals(ValidacionAeropuertoDataEnum.ERROR_CODIGO_IATA, aeropuertoService.validar(aeropuerto));
+	}
+	
 	@Test
 	void vueloValidarValidaciones(){
 		Vuelo vuelo = new Vuelo();
